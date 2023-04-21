b8a35ea1fd8da861f1f37b5f02f8b93d09b87123
bhoobalan-bhoo <bhoobalan25@gmail.com>
2023-04-21 10:58:16 +0530

    Welcome Widget Added

---
+++ resources/views/vendor/filament/widgets/filament-info-widget.blade.php
@@ -0,0 +1,15 @@
+<x-filament::widget class="filament-filament-info-widget">
+    <x-filament::card class="relative">
+        <div class="relative h-12 flex flex-col justify-center items-center space-y-2">
+
+
+            <div class="text-sm flex space-x-2 rtl:space-x-reverse">
+
+                    Welcome to Covid Vaccination Booking System
+                </a>
+            </div>
+        </div>
+
+
+    </x-filament::card>
+</x-filament::widget>
